6a68693de65dbd807c6a165b84391583853ff40f
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-02-03 15:39:15 -0500

    Base form showing

---
+++ watermyplants/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewUser from "./components/signup";
+
+function App() {
+  return (
+    <div className="App">
+    <NewUser />
+    </div>
+  );
+}
+
+export default App;
